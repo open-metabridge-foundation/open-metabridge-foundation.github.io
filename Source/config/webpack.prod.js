f186469c8a0acb82fb7d7f7100a726b13f0909d5
Yannik Goldgräbe <hi@yannik.gold>
2023-02-04 23:42:25 +0100

    Update webpack.prod.js

--- Source/config/webpack.prod.js
+++ Source/config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/',
+    publicPath: '/website',
     filename: 'js/[name].[contenthash].bundle.js',
   },
   module: {
