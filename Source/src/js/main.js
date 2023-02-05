2a8bb00c8fa5dd1e83917e576d630f68f64923c0
mlewa89 <mlewandowsky@web.de>
2023-02-05 17:37:44 +0100

    Graphics added. Further styling adjustments

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -10,3 +10,29 @@ new Typed('#typed-text', {
     startDelay: 1000,
     loop: true
 });
+
+// draw circles
+document.addEventListener("DOMContentLoaded", (event) => {
+    const canvas = document.getElementById('circle-layer');
+    const ctx = canvas.getContext('2d');
+  
+    // params: x-position, y-position, radius, line width
+    const drawCircle = (x, y, r, w) => {
+        ctx.beginPath();
+        ctx.lineWidth = w;
+        ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+    }
+  
+    // resize observer
+    const observer = new ResizeObserver(() => {
+      canvas.width  = canvas.offsetWidth;
+      canvas.height = canvas.offsetHeight;
+  
+      drawCircle(canvas.width, canvas.height, canvas.width * 0.100, canvas.width * 0.040);
+      drawCircle(canvas.width * 0.685, canvas.height / 1.7, canvas.width * 0.062, canvas.width * 0.024);
+      drawCircle(canvas.width * 0.833, 0, canvas.width * 0.040, canvas.width * 0.015);
+    });
+    observer.observe(canvas)
+  });
